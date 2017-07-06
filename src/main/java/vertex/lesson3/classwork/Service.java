053c43f7edb66f9b80b3100f28ae2ec13dca9951
Alexey Baybuzenko <alexeybaybuzenko@gmail.com>
2017-07-06 21:56:45 +0300

    Initial commit

---
+++ src/main/java/vertex/lesson3/classwork/Service.java
@@ -0,0 +1,135 @@
+package vertex.lesson3.classwork;
+
+
+public class Service {
+    //    public static void main(String[] args) {
+//        int count = 0;
+//        for (int i = 0; i<10;i++) {
+//            System.out.println("Alex Baybuzenko");
+//            count++;
+//        }
+//
+//        for (int i = 0; i<25;i++) {
+//            System.out.println("Alex Baybuzenko");
+//            count++;
+//        }
+//        System.out.println(count);
+//
+//    }
+//public static void main(String[] args) {
+//    int counter = 0;
+//    int value = 0;
+//    while (value <= 10) {
+//        value++;
+//        counter++;
+//        System.out.println(counter);
+//
+//    }
+//}
+//public static void main(String[] args) {
+//    int value = 12;
+//
+//    if (value % 2 == 0) {
+//        System.out.println("yes");
+//    } else {
+//        System.out.println(false);
+//    }
+//}
+//    public static void main(String[] args) {
+//        int counter = 0;
+//        int value = 1;
+//        while (value <= 100) {
+//            value++;
+//            counter++;
+//            if (value % 2 == 0) {
+//                System.out.println(counter + " odd");
+//            } else {
+//                System.out.println(counter + " even");
+//            }
+//        }
+//    }
+//    public static void main(String[] args) {
+//        int tracer = 0;
+//        for (int i = 0; i < 10; i++) {
+//            tracer++;
+//        }
+//        int value = 0;
+//        while (value <= 10) {
+//            tracer++;
+//            value++;
+//        }
+//        System.out.println(tracer);
+//
+//    }
+
+//    public static void main(String[] args) {
+//        int[] mas = new int[10];
+//        System.out.println(mas.length);
+
+    //        mas[3] = 3;
+//        System.out.println(mas[9]);
+//        System.out.println(mas[3]);
+//        for (int i = 0; i < mas.length; i++) {
+//            System.out.println(mas[i]);
+//        }
+//    }
+//public static void main(String[] args) {
+//    int[] mas = new int[10];                               /*внесение в массив данных*/
+//    for (int i = 0; i < mas.length; i++) {
+//        mas[i] =i;
+//    }
+//    for (int i = 0; i < mas.length; i++) {
+//        System.out.println(mas[i]);
+//    }
+//    int i = 0;
+//    while (i < mas.length) {
+//        mas[i] = 12;
+//        i++;
+//    }
+//
+//    for (int j = 0; j < mas.length; j++) {
+//        System.out.println(mas[j]);
+//    }
+//    }
+//    public static void main(String[] args) {
+//        String arr[] = {"1", "2", "3"};
+//        int[] mas = new int[arr.length];
+//        for (int i = 0; i < arr.length; i++) {
+//            mas[i] = Integer.parseInt(arr[i]);
+//        }
+//        for (int i = 0; i < mas.length; i++) {
+//            System.out.println(mas[i]);
+//        }
+//
+//    }
+    public static void main(String[] args) {
+//    int[] mas = {3,2,5,10};
+//    for (int i = 0; i < mas.length; i++) {
+//        if (mas[i]%2==0) {
+//            System.out.println(mas[i]);
+//        }
+//    }
+//}
+//        int[] mas = {2, 5, 10, 20};
+//        int[] arr = {3, 7, 8, 15,4};
+//        if (mas.length > arr.length) {
+//            System.out.println("mas bigger then arr");
+//        } else if (mas.length < arr.length) {
+//            System.out.println("mas lower then arr");
+//        }else if (mas.length == arr.length) {
+//            System.out.println("mas ravno arr");
+//        }
+//    }
+        int[] mas = {1,2,33,4};
+        for (int i = 0; i < mas.length; i++) {
+            if (mas[i] == 2 || mas[i] == 3) {
+                mas[i] = 12;
+            }
+            System.out.println(mas[i]);
+            }
+        }
+
+
+
+}
+
